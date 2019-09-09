0c54618a3a5e3dfc54b7bfdf18fde314f289e036
Geraldo B. I <geraldoarema87@gmail.com>
2019-09-09 10:25:43 +0700

    Tebak angka selesai

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -1,5 +1,6 @@
 package id.ac.polinema.tebakangka;
 
+import androidx.annotation.IntegerRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,25 +13,43 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	//  TODO: deklarasikan variabel di sini
+    //  TODO: deklarasikan variabel di sini
+    private int angka;
+    private EditText angkaInput;
+    private Button btn;
+    private int angkaTebakan;
+    Random random = new Random();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// TODO: bind layout di sini
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO: bind layout di sini
+        initRandomNumber();
+    }
 
-	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+    // TODO: generate angka random di sini
+    private void initRandomNumber() {
+        angka = random.nextInt(10) + 1;
+    }
 
-	}
+    public void handleGuess(View view) {
+        // TODO: Tambahkan logika untuk melakukan pengecekan angka
+        angkaInput = findViewById(R.id.number_input);
+        angkaTebakan = Integer.parseInt(String.valueOf(angkaInput.getText()));
+        if (angkaTebakan == angka) {
+            Toast.makeText(this, "Tebakan anda benar !!!", Toast.LENGTH_SHORT).show();
+            btn = findViewById(R.id.guess_button);
+            btn.setEnabled(false);
+        } else if (angkaTebakan > angka) {
+            Toast.makeText(this, "Tebakan anda terlalu besar !!!", Toast.LENGTH_SHORT).show();
+        } else if (angkaTebakan < angka) {
+            Toast.makeText(this, "Tebakan anda terlalu kecil !!!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
-	}
-
-	public void handleReset(View view) {
-		// TODO: Reset tampilan
-	}
+    public void handleReset(View view) {
+        // TODO: Reset tampilan
+        setContentView(R.layout.activity_main);
+    }
 }
